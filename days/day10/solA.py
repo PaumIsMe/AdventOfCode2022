76d35b9aa1ceee552c2b6fae782c56a6d01cbc4c
PaumIsMe <theapologeticweasels@gmail.com>
2022-12-10 01:26:50 -0500

    Day 10 top 400!

--- days/day10/solA.py
+++ days/day10/solA.py
@@ -1,3 +1,21 @@
 from Advent2022.helpers.parse_input import *
 
 input_arr = parse_input(10)
+
+c = 1
+t = 0
+v = 1
+
+for line in input_arr:   
+    c += 1
+    if (c % 40 == 20):
+        t += c * v
+    
+    if (line[0] == 'a'):
+        c += 1
+        v += int(line.split(" ")[1])
+        if (c % 40 == 20):
+            t += c * v
+
+print(t)
+
--- days/day10/solB.py
+++ days/day10/solB.py
@@ -1,3 +1,26 @@
 from Advent2022.helpers.parse_input import *
 
 input_arr = parse_input(10)
+
+c = 1
+v = 1
+arr = [[0 for i in range(0, 40)] for i in range(0, 6)]
+
+def draw(a, v, t):
+    x = (t - 1) % 40
+    y = (t - 1) // 40
+
+    if y < 6:
+        a[y][x] = '#' if abs(v - x) <= 1 else '.'
+
+for line in input_arr:
+    draw(arr, v, c)
+    c += 1
+
+    if line[0] == 'a':
+        draw(arr, v, c)
+        c += 1
+        v += int(line.split(" ")[1])
+
+print_array(arr)
+
